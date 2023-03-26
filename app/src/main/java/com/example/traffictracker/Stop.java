2bb640c601eb922d9c316ec349df965b195f1fe7
Stav Komarnitsky <stavko07@gmail.com>
2023-03-26 21:22:18 +0300

    Added trafficJam class (extends Stop), with slight modifications to Stop

--- app/src/main/java/com/example/traffictracker/Stop.java
+++ app/src/main/java/com/example/traffictracker/Stop.java
@@ -6,13 +6,13 @@ import java.sql.Time;
 
 public class Stop {
     private final Location stopLocation;
-    private Time stopStart;
+    private final Time stopStart;
     private Time stopEnd;
     private Time totalStopTime;
 
-    public Stop(Location stopLocation, Time stopStart){
+    public Stop(Location stopLocation){
         this.stopLocation = stopLocation;
-        this.stopStart = stopStart;
+        this.stopStart = new Time(stopLocation.getTime());
     }
 
     public void endStop(Time stopEnd) {
@@ -28,16 +28,8 @@ public class Stop {
         return stopStart;
     }
 
-    public void setStopStart(Time stopStart) {
-        this.stopStart = stopStart;
-    }
-
     public Time getStopEnd() {
         return stopEnd;
-    }
-
-    public void setStopEnd(Time stopEnd) {
-        this.stopEnd = stopEnd;
     }
 
     public Time getTotalStopTime() {
--- app/src/main/java/com/example/traffictracker/TrafficJam.java
+++ app/src/main/java/com/example/traffictracker/TrafficJam.java
@@ -1,2 +1,30 @@
-package com.example.traffictracker;public class TrafficJam {
+package com.example.traffictracker;
+
+import android.location.Location;
+
+import java.sql.Time;
+
+public class TrafficJam extends Stop {
+    private Location endLocation;
+    private double distance;
+
+    public TrafficJam(Location stopLocation) {
+        super(stopLocation);
+    }
+
+    public void endTrafficJam(Location endLocation) {
+        this.endStop(new Time(endLocation.getTime()));
+        this.endLocation = endLocation;
+        double dx = endLocation.getLongitude() - this.getStopLocation().getLongitude();
+        double dy = endLocation.getLatitude() - this.getStopLocation().getLatitude();
+        this.distance = Math.pow((dx*dx + dy*dy), 0.5);
+    }
+
+    public Location getEndLocation() {
+        return endLocation;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
 }
