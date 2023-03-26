f46fa862d9d3e4a4eae0be1adb60571c545a440f
nir kriemer <nirbur327@gmail.com>
2023-03-26 20:18:24 +0300

    added files to switch to new branch

--- app/src/main/java/com/example/traffictracker/MainActivity.java
+++ app/src/main/java/com/example/traffictracker/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.traffictracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity implements LocationListener {
+    private LocationManager locationManager;
+    private LocationListener locationListener;
     private RadioGroup transportationOptions;
     private Button startButton;
     private Button stopButton;
@@ -29,6 +38,34 @@ public class MainActivity extends AppCompatActivity {
         trafficJamButton.setVisibility(View.GONE);
 
         startButton.setBackgroundColor(getResources().getColor(android.R.color.holo_green_dark));
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // locationListener = new LocationListener();
+            //  @Override
+            //  public void onLocationChanged(Location location) {
+            //      // Do something with the location
+            //  }
+            //
+            //  @Override
+            //  public void onStatusChanged(String provider, int status, Bundle extras) {
+            //  }
+            //
+            //  @Override
+            //  public void onProviderEnabled(String provider) {
+            //  }
+            //
+            //  @Override
+            //  public void onProviderDisabled(String provider) {
+            //  }
+         //};
+         // if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+         //         && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+         //     // Request location permissions
+         //     ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+         // } else {
+         //     // Start location updates
+         //     locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+         // }
 
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -101,5 +138,26 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        // @Override
+        // public void onLocationChanged(Location location) {
+        //     double latitude = location.getLatitude();
+        //     double longitude = location.getLongitude();
+        //
+        //     // Do something with the latitude and longitude
+        // }
+    }
+
+    public void getLocation(View view)
+    {
+
+        // if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LCOATION) == PackageManager.P)
+        // {
+        //
+        // }
+    }
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+
     }
 }
