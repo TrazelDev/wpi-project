a83fa3996b7bb23bd1aaab6c57dbf79fed75e572
nir kriemer <nirbur327@gmail.com>
2023-03-26 21:46:38 +0300

    started to check how to get time and location

--- app/src/main/java/com/example/traffictracker/MainActivity.java
+++ app/src/main/java/com/example/traffictracker/MainActivity.java
@@ -2,19 +2,34 @@ package com.example.traffictracker;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.Manifest;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
 
 
 public class MainActivity extends AppCompatActivity implements LocationListener {
@@ -24,11 +39,13 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     private Button startButton;
     private Button stopButton;
     private Button trafficJamButton;
+    private TextView checking;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity_layout);
+        checking = findViewById(R.id.textView1);
 
         transportationOptions = findViewById(R.id.transportation_options);
         startButton = findViewById(R.id.start_button);
@@ -40,32 +57,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         startButton.setBackgroundColor(getResources().getColor(android.R.color.holo_green_dark));
 
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        // locationListener = new LocationListener();
-            //  @Override
-            //  public void onLocationChanged(Location location) {
-            //      // Do something with the location
-            //  }
-            //
-            //  @Override
-            //  public void onStatusChanged(String provider, int status, Bundle extras) {
-            //  }
-            //
-            //  @Override
-            //  public void onProviderEnabled(String provider) {
-            //  }
-            //
-            //  @Override
-            //  public void onProviderDisabled(String provider) {
-            //  }
-         //};
-         // if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-         //         && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-         //     // Request location permissions
-         //     ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
-         // } else {
-         //     // Start location updates
-         //     locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-         // }
+        getLocation();
 
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -148,14 +140,71 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         // }
     }
 
-    public void getLocation(View view)
-    {
-
-        // if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LCOATION) == PackageManager.P)
-        // {
-        //
-        // }
+    public void getLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            retrieveLocation();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 200);
+        }
     }
+
+    @SuppressLint("MissingPermission")
+    private void retrieveLocation() {
+        checking.setText("work");
+        LocationManager manger = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        manger.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, this);
+
+        Location location = manger.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        if(location != null)
+        {
+            double lat = location.getLatitude();
+            double longitude = location.getLongitude();
+
+            // geocoder is for converting
+            Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+
+
+            try {
+                List<Address> addressList = geocoder.getFromLocation(lat, longitude, 1);
+
+                // getting the time:
+                Calendar cal = Calendar.getInstance();
+                TimeZone timeZone = TimeZone.getDefault();
+                Date date = cal.getTime();
+                // Add three hours to the local time
+                cal.setTime(date);
+                cal.add(Calendar.HOUR_OF_DAY, 3);
+                date = cal.getTime();
+
+                String timeString = DateFormat.getTimeInstance().format(date);
+
+                checking.setText("lay:" + lat + " long: " + longitude + " time: " + timeString);
+            } catch (IOException e) {
+                e.printStackTrace();
+                checking.setText("fail");
+            }
+        }
+
+    }
+
+
+    public void onRequestPermissionResult(int requestCode, @NonNull String[] permission, int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permission, grantResults);
+
+
+        if(requestCode == 200 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+        {
+            retrieveLocation();
+        }
+        else
+        {
+            // add something:
+        }
+    }
+
     @Override
     public void onLocationChanged(@NonNull Location location) {
 
