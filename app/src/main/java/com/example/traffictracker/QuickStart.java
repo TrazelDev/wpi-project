dc4a8aeee43a8fbfe659a0ed6ee9f0d306b96fd7
Stav Komarnitsky <stavko07@gmail.com>
2023-04-22 20:56:10 +0300

    actually authenticated correctly

--- app/src/main/java/com/example/traffictracker/QuickStart.java
+++ app/src/main/java/com/example/traffictracker/QuickStart.java
@@ -1,20 +1,23 @@
 package com.example.traffictracker;
-
 import static com.mongodb.client.model.Filters.eq;
+
 import org.bson.Document;
+
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
 public class QuickStart {
-
     public static void main( String[] args ) {
+
         // Replace the placeholder with your MongoDB deployment's connection string
-        String uri = "mongodb+srv://TrafficTracker:<password>@cluster0.ltkn6xs.mongodb.net/?retryWrites=true&w=majority";
+        String uri = "mongodb+srv://TrafficTracker:wpi_project@cluster0.ltkn6xs.mongodb.net/?retryWrites=true&w=majority";
+
         try (MongoClient mongoClient = MongoClients.create(uri)) {
             MongoDatabase database = mongoClient.getDatabase("sample_mflix");
             MongoCollection<Document> collection = database.getCollection("movies");
+
             Document doc = collection.find(eq("title", "Back to the Future")).first();
             if (doc != null) {
                 System.out.println(doc.toJson());
